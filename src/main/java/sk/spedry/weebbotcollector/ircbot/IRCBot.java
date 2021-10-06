ba5d243eb92f28e61bb0347774429dc9fe6e3f18
Spedry <spedrysk@gmail.com>
2021-10-06 06:01:59 +0200

    added listener and command class

--- src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBot.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBot.java
@@ -4,20 +4,30 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.jmx.Server;
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
 import org.pircbotx.exception.IrcException;
 import org.pircbotx.hooks.ListenerAdapter;
 import sk.spedry.weebbotcollector.ircbot.util.BotInfo;
+import sk.spedry.weebbotcollector.util.WCMServer;
+import sk.spedry.weebbotcollector.util.lists.ServerList;
+import sk.spedry.weebbotcollector.work.WBCWorkPlace;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
 
 public class IRCBot extends ListenerAdapter implements Runnable {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
     private final PircBotX bot;
-
+    private final IRCBotCommands botCommands;
+    private final IRCBotListener botListener;
+    private WBCWorkPlace workPlace;
     @Getter
     private final String threadName;
     @Getter
@@ -33,13 +43,16 @@ public class IRCBot extends ListenerAdapter implements Runnable {
     @Setter
     private String downloadFolder;
 
-    public IRCBot(BotInfo botInfo) {
+    public IRCBot(BotInfo botInfo, WBCWorkPlace workPlace) {
         this.threadName = botInfo.getServerName() + "_" + botInfo.getChannelName() + "_" + botInfo.getUserName();
         this.userName = botInfo.getUserName();
         this.serverName = botInfo.getServerName();
         this.channelName = botInfo.getChannelName();
         this.downloadFolder = botInfo.getDownloadFolder();
+        this.workPlace = workPlace;
         bot = new PircBotX(configureBot());
+        botCommands = new IRCBotCommands(bot);
+        botListener = new IRCBotListener(downloadFolder, workPlace, botCommands);
     }
 
     public final Configuration configureBot() {
@@ -47,10 +60,12 @@ public class IRCBot extends ListenerAdapter implements Runnable {
         return new Configuration.Builder()
                 .setName(userName)
                 .setAutoNickChange(true)
-                .addServer(serverName)
-                .addAutoJoinChannel(channelName)
+                //.addServer()
+                .addServers()
+                //.addAutoJoinChannel(channelName)
+                .addAutoJoinChannels()
                 .setAutoReconnect(true)
-                .addListener(new IRCBotListener(downloadFolder))
+                .addListener(botListener)
                 //TODO TEST ON RASPBERRY PI 4 8GB
                 .setDccTransferBufferSize(1024*5)
                 .setAutoReconnectDelay(() -> 60)
@@ -60,6 +75,15 @@ public class IRCBot extends ListenerAdapter implements Runnable {
                 .setShutdownHookEnabled(true)
                 .buildConfiguration();
     }
+
+    /*private Iterable<Configuration.ServerEntry> getServerList() {
+        List<Configuration.ServerEntry> serverList = new ArrayList<Configuration.ServerEntry>();
+        for (WCMServer server : workPlace.getServerList(workPlace.getServerListFile()).getServerList()) {
+            serverList.add(new Configuration.ServerEntry(server.getServerName()));
+        }
+
+        return serverList;
+    }*/
 
     @Override
     public void run() {
