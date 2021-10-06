20e35964805b420af7aa6fd5ee329927185d5457
Spedry <spedrysk@gmail.com>
2021-10-06 06:04:14 +0200

    added WBCService -> will manage bot. needs to be changed

--- src/main/java/sk/spedry/weebbotcollector/ircbot/WBCService.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/WBCService.java
@@ -3,14 +3,17 @@ package sk.spedry.weebbotcollector.ircbot;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import sk.spedry.weebbotcollector.ircbot.util.BotInfo;
+import sk.spedry.weebbotcollector.work.WBCWorkPlace;
 
 public class WBCService {
 
     private final Logger logger = LogManager.getLogger(this.getClass());
 
-    public void createBot(BotInfo botInfo) {
-        Thread botThread = new Thread(new IRCBot(botInfo));
+    public void createBot(BotInfo botInfo, WBCWorkPlace workPlace) {
+        logger.trace("Creating IRC bot");
+        Thread botThread = new Thread(new IRCBot(botInfo, workPlace));
         botThread.setDaemon(true);
+        logger.debug("Starting IRC bot thread");
         botThread.start();
     }
 }
