68a47ed3ee65e89877acceba15a462d6e421c544
Spedry <spedrysk@gmail.com>
2021-10-23 19:37:25 +0200

    WCMProgress object

---
+++ src/main/java/sk/spedry/weebbotcollector/util/WCMProgress.java
@@ -0,0 +1,10 @@
+package sk.spedry.weebbotcollector.util;
+
+public class WCMProgress {
+
+    private double progress;
+
+    public WCMProgress(double progress) {
+        this.progress =  progress;
+    }
+}
