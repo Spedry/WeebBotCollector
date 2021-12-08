e5ca3337ad967d4f7654b9ca20a310c709dd0b66
Spedry <spedrysk@gmail.com>
2021-12-08 23:25:41 +0100

    WCMAnimeName

---
+++ src/main/java/sk/spedry/weebbotcollector/util/WCMAnimeName.java
@@ -0,0 +1,12 @@
+package sk.spedry.weebbotcollector.util;
+
+import lombok.Getter;
+
+public class WCMAnimeName {
+    @Getter
+    private String animName;
+
+    public WCMAnimeName(String animName) {
+        this.animName = animName;
+    }
+}
