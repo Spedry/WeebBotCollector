c31fcf6a9f30dc1c22874ecc129e1c9071d059aa
Spedry <spedrysk@gmail.com>
2021-12-17 19:55:27 +0100

    rework of DownloadMessage class

--- src/main/java/sk/spedry/weebbotcollector/ircbot/util/DownloadMessage.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/util/DownloadMessage.java
@@ -1,17 +1,28 @@
 package sk.spedry.weebbotcollector.ircbot.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
-@AllArgsConstructor
 public class DownloadMessage {
     @Getter
-    @Setter
-    private String botName;
+    private final String wholeMessage;
     @Getter
-    @Setter
-    private String message;
+    private final String botName;
+    @Getter
+    private final String message;
     @Getter
     private String animeName;
+
+    public DownloadMessage(String message) {
+        this.wholeMessage = message;
+        String[] spliced = message.split(" ");
+        this.botName = spliced[1];
+        this.message = spliced[2] + " " + spliced[3] + " " + spliced[4];
+    }
+
+    public DownloadMessage(String botName, String message, String animeName) {
+        this.wholeMessage = "/msg " + botName + " " + message;
+        this.botName = botName;
+        this.message = message;
+        this.animeName = animeName;
+    }
 }
