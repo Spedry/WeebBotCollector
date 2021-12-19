1f111d6db5198142ed3de0866b334b33007bc645
Spedry <spedrysk@gmail.com>
2021-12-19 23:50:14 +0100

    WCMDownAlrRel to store data

---
+++ src/main/java/sk/spedry/weebbotcollector/util/WCMDownAlrRel.java
@@ -0,0 +1,10 @@
+package sk.spedry.weebbotcollector.util;
+
+import lombok.Getter;
+
+public class WCMDownAlrRel {
+    @Getter
+    private boolean download;
+    @Getter
+    private int alreadyReleasedEp;
+}
