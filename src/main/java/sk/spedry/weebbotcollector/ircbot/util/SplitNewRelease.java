f5ba6de37df22d9933ff07188b5cbb60d979ac13
Spedry <spedrysk@gmail.com>
2022-01-13 22:12:54 +0100

    SplitNewRelease now extends Release

--- src/main/java/sk/spedry/weebbotcollector/ircbot/util/SplitNewRelease.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/util/SplitNewRelease.java
@@ -6,9 +6,7 @@ import lombok.NonNull;
 import sk.spedry.weebbotcollector.util.WCMAnime;
 import sk.spedry.weebbotcollector.util.lists.AnimeList;
 
-public class SplitNewRelease {
-    @Getter
-    private final String message;
+public class SplitNewRelease extends Release {
     @Getter
     private final String info;
     @Getter
@@ -16,24 +14,15 @@ public class SplitNewRelease {
     @Getter
     private final String animeName;
     @Getter
-    private String animeFolderName = null;
-    @Getter
     private final DownloadMessage downloadMessage;
 
     public SplitNewRelease(String message) {
+        super(message);
         String[] spliced = message.split("\\*");
-        this.message = message;
         this.info = spliced[0].trim();
         this.size = spliced[1].trim();
         this.animeName = spliced[2].trim();
         this.downloadMessage = new DownloadMessage(spliced[3].trim());
     }
 
-    public void setAnimeFolderName(@NonNull AnimeList animeList) {
-        for (WCMAnime anime : animeList.getAnimeList()) {
-            if (animeName.contains(anime.getAnimeName().toLowerCase())) {
-                animeFolderName = anime.getAnimeName();
-            }
-        }
-    }
 }
