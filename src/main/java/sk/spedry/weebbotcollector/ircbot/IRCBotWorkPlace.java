623aca5c4b0bb5237f85778e3f379d4609dadd1f
Spedry <spedrysk@gmail.com>
2022-01-13 22:11:49 +0100

    created isDownload function

--- src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotWorkPlace.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotWorkPlace.java
@@ -1,6 +1,22 @@
 package sk.spedry.weebbotcollector.ircbot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import sk.spedry.weebbotcollector.ircbot.util.SplitAlreadyReleased;
+import sk.spedry.weebbotcollector.ircbot.util.SplitNewRelease;
+import sk.spedry.weebbotcollector.work.WBCWorkPlace;
+
+import java.io.File;
+
 public class IRCBotWorkPlace {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    private final WBCWorkPlace workPlace;
+
+    public IRCBotWorkPlace(WBCWorkPlace workPlace) {
+        this.workPlace = workPlace;
+    }
 
     public String bytesToReadable(long bytes) {
         double size_kb = bytes / 1024;
@@ -14,5 +30,36 @@ public class IRCBotWorkPlace {
         } else {
             return String.format("%.3f", size_gb) + " KB";
         }
+    }
+
+    public boolean isDownloaded(Object t) {
+        String animeFolderName, animeName;
+        if (t instanceof SplitNewRelease) {
+            logger.debug("Object t is instance of SplitNewRelease");
+            SplitNewRelease newRelease = (SplitNewRelease) t;
+            animeFolderName = newRelease.getAnimeFolderName();
+            animeName = newRelease.getAnimeName();
+        }
+        else if (t instanceof  SplitAlreadyReleased) {
+            logger.debug("Object t is instance of SplitAlreadyReleased");
+            SplitAlreadyReleased alreadyReleased = (SplitAlreadyReleased) t;
+            animeFolderName = alreadyReleased.getAnimeFolderName();
+            animeName = alreadyReleased.getAnimeName();
+        }
+        else {
+            logger.error("Object t has wrong instance");
+            return false;
+        }
+        File folder = new File(workPlace.getConf().getProperty("downloadFolder") + "/" + animeFolderName);
+        File[] listOfFiles = folder.listFiles();
+        assert listOfFiles != null;
+        if (folder.exists()) {
+            for (File file : listOfFiles) {
+                if (animeName.contains(file.getName().toLowerCase())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
