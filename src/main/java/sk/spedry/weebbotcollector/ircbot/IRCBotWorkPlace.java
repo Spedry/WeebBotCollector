094738822dd1b75257a825f6a1b884e15104287a
Spedry <spedrysk@gmail.com>
2022-04-28 22:55:36 +0200

    moved isDownloaded to testFunctions class and added trace log

--- src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotWorkPlace.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotWorkPlace.java
@@ -15,54 +15,23 @@ public class IRCBotWorkPlace {
     private final WBCWorkPlace workPlace;
 
     public IRCBotWorkPlace(WBCWorkPlace workPlace) {
+        logger.traceEntry();
         this.workPlace = workPlace;
+        logger.traceExit();
     }
 
     public String bytesToReadable(long bytes) {
+        logger.traceEntry();
         double size_kb = bytes / 1024;
         double size_mb = size_kb / 1024;
         double size_gb = size_mb / 1024 ;
 
         if (size_gb > 0){
-            return String.format("%.3f", size_gb) + " GB";
+            return logger.traceExit(String.format("%.3f", size_gb) + " GB");
         } else if(size_mb > 0) {
-            return String.format("%.3f", size_gb) + " MB";
+            return logger.traceExit(String.format("%.3f", size_gb) + " MB");
         } else {
-            return String.format("%.3f", size_gb) + " KB";
+            return logger.traceExit(String.format("%.3f", size_gb) + " KB");
         }
-    }
-
-    public boolean isDownloaded(Object t) {
-        String animeFolderName, animeName;
-        if (t instanceof SplitNewRelease) {
-            logger.debug("Object t is instance of SplitNewRelease");
-            SplitNewRelease newRelease = (SplitNewRelease) t;
-            animeFolderName = newRelease.getAnimeFolderName();
-            animeName = newRelease.getAnimeName();
-        }
-        else if (t instanceof  SplitAlreadyReleased) {
-            logger.debug("Object t is instance of SplitAlreadyReleased");
-            SplitAlreadyReleased alreadyReleased = (SplitAlreadyReleased) t;
-            animeFolderName = alreadyReleased.getAnimeFolderName();
-            animeName = alreadyReleased.getAnimeName();
-        }
-        else {
-            logger.error("Object t has wrong instance");
-            return false;
-        }
-        File folder = new File(workPlace.getConf().getProperty("downloadFolder") + "/" + animeFolderName);
-        File[] listOfFiles = folder.listFiles();
-        assert listOfFiles != null;
-        if (folder.exists()) {
-            for (File file : listOfFiles) {
-                if (animeName.contains(file.getName().toLowerCase())) {
-                    return true;
-                }
-            }
-        }
-        else {
-            logger.error("Folder didn't exist");
-        }
-        return false;
     }
 }
