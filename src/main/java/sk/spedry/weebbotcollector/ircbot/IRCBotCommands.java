f118e6b15572ba45c0614895d92dc858c8610679
Spedry <spedrysk@gmail.com>
2022-04-28 23:06:29 +0200

    added trace log

--- src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotCommands.java
+++ src/main/java/sk/spedry/weebbotcollector/ircbot/IRCBotCommands.java
@@ -22,27 +22,35 @@ public class IRCBotCommands {
     }
 
     public void sendMessage(String target, String message) {
+        logger.traceEntry("To: {} : {}", target, message);
         logger.info("Sending message: " + message + " to: " + target);
         bot.sendIRC().message(target, message);
+        logger.traceExit();
     }
 
     public void sendMessage(DownloadMessage message) {
-        logger.info("Sending message: {} to: {}, to download: {}", message.getMessage(), message.getBotName(), message.getAnimeName());
+        logger.traceExit(message);
+        logger.info("Sending message: {} to: {}, to download: {}", message.getMessage(), message.getBotName(), message.getAnimeFileName());
         bot.sendIRC().message(message.getBotName(), message.getMessage());
+        logger.traceExit();
     }
 
     public void searchAnime(String target, String animeName, String numberOfEp, String quality) {
+        logger.traceEntry("To: {}", target);
         String searchFor = "!s " + animeName + " " + numberOfEp + " " + quality;
         sendMessage(target, searchFor);
+        logger.traceExit(searchFor);
     }
 
     public void searchAnime(String target, String downloadFrom, String animeName, String numberOfEp, String quality) {
+        logger.traceEntry("To: {}", target);
         if (Objects.equals(downloadFrom, "")) {
             searchAnime(target, animeName, numberOfEp, quality);
             return;
         }
         String searchFor = "!s " + downloadFrom + " " + animeName + " " + numberOfEp + " " + quality;
         sendMessage(target, searchFor);
+        logger.traceExit(searchFor);
     }
 
     public ArrayList<DownloadMessage> getDownloadQueueList() {
