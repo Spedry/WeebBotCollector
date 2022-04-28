9b486f178e5f758c2827943b04bdb3c4df99cb7c
Spedry <spedrysk@gmail.com>
2022-04-28 22:53:53 +0200

    added logger

--- src/main/java/sk/spedry/weebbotcollector/util/lists/ServerList.java
+++ src/main/java/sk/spedry/weebbotcollector/util/lists/ServerList.java
@@ -1,11 +1,16 @@
 package sk.spedry.weebbotcollector.util.lists;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import sk.spedry.weebbotcollector.util.WCMServer;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ServerList {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
     private final List<WCMServer> list;
 
     public ServerList() {
